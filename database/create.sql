b7f823a65fc585cd64d29747b45a05df39d0d53d
Eduardo Favarato <eduardo.favarato@gmail.com>
2022-11-30 22:37:09 -0300

    create database creation query

---
+++ database/create.sql
@@ -0,0 +1,5 @@
+-- Active: 1669857720167@@pelada-futsal-db.ckbzgbugj70p.sa-east-1.rds.amazonaws.com@5432@pelada
+CREATE TABLE PLAYER(
+   ID       SERIAL  PRIMARY KEY NOT NULL,
+   NAME     TEXT    NOT NULL
+);
