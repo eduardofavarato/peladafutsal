e50056b4d267fa3f0b67a610309c95f205d23080
Eduardo Favarato <eduardo.favarato@gmail.com>
2022-12-07 12:41:38 -0300

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Link, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Players from "./features/players/Players";
+import NavBar from "./components/NavBar";
 
 function App() {
 	return (
 		<div>
-			<nav className="navbar navbar-expand navbar-dark bg-dark">
-				<a href="/standings" className="navbar-brand">
-					Classificação
-				</a>
-				<div className="navbar-nav mr-auto">
-					<li className="nav-item">
-						<Link to={"/players"} className="nav-link">
-							Jogadores
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/match"} className="nav-link">
-							Partida
-						</Link>
-					</li>
-				</div>
-			</nav>
+			<NavBar />
 
 			<div className="container mt-3">
 				<Routes>
@@ -31,6 +17,48 @@ function App() {
 				</Routes>
 			</div>
 		</div>
+
+		// <div>
+		// 	<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		// 		<a href="/standings" className="navbar-brand">
+		// 			Classificação
+		// 		</a>
+		// 		<button
+		// 			className="navbar-toggler"
+		// 			type="button"
+		// 			data-toggle="collapse"
+		// 			data-target="#navbarSupportedContent"
+		// 			aria-controls="navbarSupportedContent"
+		// 			aria-expanded="false"
+		// 			aria-label="Toggle navigation"
+		// 		>
+		// 			<span className="navbar-toggler-icon"></span>
+		// 		</button>
+
+		// 		<div className="collapse navbar-collapse" id="navbarSupportedContent">
+		// 			<ul className="navbar-nav mr-auto">
+		// 				<li className="nav-item">
+		// 					<Link to={"/players"} className="nav-link">
+		// 						Jogadores
+		// 					</Link>
+		// 				</li>
+		// 				<li className="nav-item">
+		// 					<Link to={"/match"} className="nav-link">
+		// 						Partida
+		// 					</Link>
+		// 				</li>
+		// 			</ul>
+		// 		</div>
+		// 	</nav>
+
+		// 	<div className="container mt-3">
+		// 		<Routes>
+		// 			{/* <Route path="/standings" element={<Standings />} /> */}
+		// 			<Route path="/players" element={<Players />} />
+		// 			{/* <Route path="/match" element={<Match />} />} /> */}
+		// 		</Routes>
+		// 	</div>
+		// </div>
 	);
 }
 
