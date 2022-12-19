1b5bcd3917a11345ccc13c74d28f3e9fd944467f
Eduardo Favarato <eduardo.favarato@gmail.com>
2022-12-19 18:04:37 -0300

    cleanup PlayerService

--- src/services/PlayerService.ts
+++ src/services/PlayerService.ts
@@ -17,38 +17,18 @@ const getAll = (successCallback?: any, errorCallback?: any) => {
 	return processApiCall(http.get, "/v1/players", null, (response: any) => successCallback(response.data), errorCallback);
 };
 
-const get = (id: any, successCallback?: any, errorCallback?: any) => {
-	return processApiCall(http.get, `/v1/players/${id}`, null, successCallback, errorCallback);
-};
-
 const create = (data: IPlayerData, successCallback?: any, errorCallback?: any) => {
 	return processApiCall(http.post, "/v1/players", data, successCallback, errorCallback);
-};
-
-const update = (id: any, data: IPlayerData, successCallback?: any, errorCallback?: any) => {
-	return processApiCall(http.put, `/v1/players/${id}`, data, successCallback, errorCallback);
 };
 
 const remove = (id: any, successCallback?: any, errorCallback?: any) => {
 	return processApiCall(http.delete, `/v1/players/${id}`, null, successCallback, errorCallback);
 };
 
-const removeAll = (successCallback?: any, errorCallback?: any) => {
-	return processApiCall(http.delete, `/v1/players`, null, successCallback, errorCallback);
-};
-
-const findByName = (name: string, successCallback?: any, errorCallback?: any) => {
-	return processApiCall(http.get, `/v1/players?name=${name}`, null, successCallback, errorCallback);
-};
-
 const PlayerService = {
 	getAll,
-	get,
 	create,
-	update,
 	remove,
-	removeAll,
-	findByName,
 };
 
 export default PlayerService;
